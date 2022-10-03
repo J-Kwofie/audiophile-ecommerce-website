a51b93e1e62e21090c55512db9a2ffcc4781fe58
J-Kwofie <judekwofie.gh@gmail.com>
2022-10-03 10:04:55 +0000

    Structure project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+function NavItem() {
+    return (
+        <li>
+    		<Link to='/'></Link>
+    	</li> 
+    )
+}
+export default function Navbar() {
+    return (
+        <div>
+			
+		</div>
+    )
+}
